9979a99b8d20d41eef2b29ddc387da0d7a8914d1
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-01-17 14:01:30 -0500

    hgfhjgvfjhdhfds

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+    >
+      Team Rocket Online
+    </h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
