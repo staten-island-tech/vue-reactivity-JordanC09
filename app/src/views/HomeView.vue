be733e7772402d2e4e3934e3831673e41da8d9d1
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-01-23 18:09:12 -0500

    bruh

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <h1
-      class="text"
-    >
-      jhgjhg
+    <h1 class="text">
+      gghgv
     </h1>
+    <div class="flex">
+      <div class="item">01</div>
+      <div class="item">02</div>
+      <div class="item">03</div>
+    </div>
+    
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import { ref, reactive } from 'vue'
+  
+  
+  
+  //localStorage.setItem();
+  
+  
+  
+</script>
 
-<style scoped></style>
+<style scoped>
+  
+</style>
