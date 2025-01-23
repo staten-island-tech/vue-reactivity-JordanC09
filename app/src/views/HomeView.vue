745d3fec78a350028aa7b3473c173a4b8bc8838b
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-01-23 07:48:50 -0500

    aef

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h1
       class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
     >
-      Team Rocket Online
+      jhgjhg
     </h1>
   </div>
 </template>
