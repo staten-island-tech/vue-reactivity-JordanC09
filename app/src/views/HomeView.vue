babe844d2673ce003f1b8295e90abcaee65d6d06
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-01-31 14:02:51 -0500

    jhsadyjhsgf

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <h1 class="text bg-red-500">gghgv</h1>
-    <div class="flex">
-      <div class="w-14 flex-1 border-red-500">01</div>
-      <div class="w-64 flex-1">02</div>
-      <div class="w-32 flex-1">03</div>
+  <body>
+    <div>
+      <h1 class="text bg-red-500">gghgv</h1>
+      <div class="flex">
+        <div class="w-14 border-4 flex-1 bg-red-500">01</div>
+        <div class="w-64 flex-1">02</div>
+        <div class="w-32 flex-1">03</div>
+      </div>
+      <button class="btn btn-outline btn-secondary">Secondary</button>
     </div>
-    <button class="btn btn-outline btn-secondary">Secondary</button>
-  </div>
+  </body>
 </template>
 
 <script setup>
